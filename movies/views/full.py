1c85364093103da1e47ebd8e7649182fb3dde078
Carmen Alvarez <carmen@rmen.ca>
2024-08-19 22:49:45 +0200

    Add, in a coment, the needed methods to avoid the N+1 query on the full page.

--- movies/views/full.py
+++ movies/views/full.py
@@ -4,6 +4,7 @@ from movies.models import Movie
 
 
 def full(request):
+    # movies = Movie.objects.select_related("studio").select_related("finance").prefetch_related("actor_set").all()
     movies = Movie.objects.all()
     context = {
         "movie_list": movies,
